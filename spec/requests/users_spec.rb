17cad12f2e7c29d90e61f69e0c17fb1f1cc6d051
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-06-02 21:01:43 -0300

    Melhorando o teste com a Gem Factory Bot

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  it "Is invalid if the level is not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
-
-    expec(user).to_not be_valid
-  end 
-
-  
+  describe "GET /users" do
+    it "works! (now write some real specs)" do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
